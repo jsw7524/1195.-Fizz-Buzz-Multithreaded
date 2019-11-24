50e999162160144bc70e10b8e26ed176cadfc963
netsp <netsp@DESKTOP-K85MK10>
2019-11-24 15:53:52 +0800

    executable

--- 1195. Fizz Buzz Multithreaded/Program.cs
+++ 1195. Fizz Buzz Multithreaded/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace _1195.Fizz_Buzz_Multithreaded
@@ -10,7 +11,7 @@ namespace _1195.Fizz_Buzz_Multithreaded
     {
         private int n;
         private readonly object balanceLock = new object();
-        int i ;
+        int i;
         public FizzBuzz(int n)
         {
             this.n = n;
@@ -28,7 +29,7 @@ namespace _1195.Fizz_Buzz_Multithreaded
                     {
                         break;
                     }
-                    if ((i % 3 == 0 && i%5!=0))
+                    if ((i % 3 == 0 && i % 5 != 0))
                     {
                         printFizz();
                         i++;
@@ -49,7 +50,7 @@ namespace _1195.Fizz_Buzz_Multithreaded
                     {
                         break;
                     }
-                    if ((i % 5 == 0 && i% 3!= 0))
+                    if ((i % 5 == 0 && i % 3 != 0))
                     {
                         printBuzz();
                         i++;
@@ -87,11 +88,11 @@ namespace _1195.Fizz_Buzz_Multithreaded
             {
                 lock (balanceLock)
                 {
-                    if (i>n)
+                    if (i > n)
                     {
                         break;
                     }
-                    if (!(i%3==0 || i%5==0))
+                    if (!(i % 3 == 0 || i % 5 == 0))
                     {
                         printNumber(i);
                         i++;
@@ -105,8 +106,27 @@ namespace _1195.Fizz_Buzz_Multithreaded
 
     class Program
     {
+        private delegate void NumberDelegate(Action<int> printNumber);
+        private delegate void BuzzDelegate(Action printNumber);
+        private delegate void FizzDelegate(Action printNumber);
+        private delegate void FizzbuzzDelegate(Action printNumber);
+
         static void Main(string[] args)
         {
+            int n = 15;
+            FizzBuzz fizzBuzz = new FizzBuzz(n);
+
+            NumberDelegate fizzBuzzNumber = new NumberDelegate(fizzBuzz.Number);
+            BuzzDelegate fizzBuzzBuzz = new BuzzDelegate(fizzBuzz.Buzz);
+            FizzDelegate fizzBuzzFizz = new FizzDelegate(fizzBuzz.Fizz);
+            FizzbuzzDelegate fizzBuzzFizzbuzz = new FizzbuzzDelegate(fizzBuzz.Fizzbuzz);
+
+            fizzBuzzNumber.BeginInvoke(x => { Console.WriteLine(x); },null,null);
+            fizzBuzzBuzz.BeginInvoke(() => { Console.WriteLine("Buzz"); },null,null);
+            fizzBuzzFizz.BeginInvoke(() => { Console.WriteLine("Fizz"); }, null, null);
+            fizzBuzzFizzbuzz.BeginInvoke(() => { Console.WriteLine("Fizzbuzz"); }, null, null);
+
+            Console.ReadKey();
         }
     }
 }
